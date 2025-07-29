456f05de7e301af89c935ecfcc6e4df81154ee54
janwit.si <janescience.top@gmail.com>
2025-07-29 12:57:09 +0700

    Fix bug

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-const withPWA = isProd ? require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true
-}) : (config) => config;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig = withPWA({
-  reactStrictMode: true,
-  env: {
-    // NEXT_PUBLIC_API_URL: 'https://your-api-url.com',
-  },
-  // rewrites, redirects, etc. สามารถใส่ได้เหมือนเดิม
+  reactStrictMode: true,
+  env: {
+    // NEXT_PUBLIC_API_URL: 'https://your-api-url.com',
+  },
+  // rewrites, redirects, etc. สามารถใส่ได้เหมือนเดิม
 });
 
 module.exports = nextConfig;
